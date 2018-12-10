02545995cd5616524084c9c56e36a2bc39e61886
Loner1024 <xth12138@gmail.com>
2018-12-10 20:15:46 +0800

    first commit

---
+++ dm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time, sys
+
+from danmu import DanMuClient
+
+def pp(msg):
+    print(msg.encode(sys.stdin.encoding, 'ignore').
+        decode(sys.stdin.encoding))
+
+dmc = DanMuClient('http://www.douyu.com/610588')
+if not dmc.isValid(): print('Url not valid')
+
+@dmc.danmu
+def danmu_fn(msg):
+    #pp('[%s] %s' % (msg['NickName'], msg['Content']))
+    print(msg['Content'])
+    with open('1.txt','ab') as f:
+        dm_text = str(msg['Content'].encode('utf-8'))
+        f.write(dm_text)
+        f.close()
+
+
+
+'''
+@dmc.gift
+def gift_fn(msg):
+    pp('[%s] sent a gift!' % msg['NickName'])
+'''
+
+dmc.start(blockThread = True)
